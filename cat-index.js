3b058b9a7da59ccf2fa383ba670f940cdbe11f14
BennyBoi365 <scepninja555@gmail.com>
2024-11-20 22:28:20 -0800

    Update cat-index.js

--- cat-index.js
+++ cat-index.js
@@ -2,6 +2,7 @@ $("#submit").click(function(){
     // Input name
     var catName = $("#catName").val();
     console.log("Cat's Name:", catName);
+   
     // Selected breed
     var selectedBreed = $("#breedOptions").val();
     console.log("Selected Breed:", selectedBreed);
