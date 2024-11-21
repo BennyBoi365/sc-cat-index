9e79d834bf01cbdaed6c8e0ad7f98390a601e7ae
BennyBoi365 <scepninja555@gmail.com>
2024-11-21 00:41:19 -0800

    cat-color

--- cat-index.js
+++ cat-index.js
@@ -6,4 +6,16 @@ $("#submit").click(function(){
     // Selected breed
     var selectedBreed = $("#breedOptions").val();
     console.log("Selected Breed:", selectedBreed);
+
+    // Input color
+    var catColor = $("#catColor").val();
+    console.log("Cat's Color:", catColor);
+
+    // Yes or No tags selection
+    var selectedTags = $("#tagsYesOrNo").val();
+    console.log("Does they have tags?:", selectedTags);
+
+    // Selected last time fed
+    var lastFed = $("#lastFed").val();
+    console.log("Last time fed:", lastFed);
 });
